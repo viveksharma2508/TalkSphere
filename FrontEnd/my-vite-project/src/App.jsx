d905fbbbe97fb16494d0f2dd46bd571ac5c2df55
viveksharma2525 <sharma.vivek0910@outlook.com>
2025-01-13 19:09:24 +0530

    update

--- FrontEnd/my-vite-project/src/App.jsx
+++ FrontEnd/my-vite-project/src/App.jsx
@@ -5,7 +5,7 @@ import ChatPage from './Components/ChatPage';
 import { io } from 'socket.io-client';
 
 const App = () => {
-  const socket = io('http://localhost:3000'); // Adjust your server URL as needed
+  const socket = io('https://talksphere-1.onrender.com'); // Adjust your server URL as needed
 
   return (
     <Router basename="/"> {/* Add basename prop */}
